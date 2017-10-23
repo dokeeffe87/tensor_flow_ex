74afad0f7cce83875b27add0dd4d8af85e6cf90c
dokeeffe87 <daniel@Daniels-MacBook-Pro.local>
2017-10-23 00:44:02 -0400

    2017-10-23

---
+++ second_pass_tensorboard.py
@@ -0,0 +1,11 @@
+import tensorflow as tf 
+
+a  =  tf.constant([2, 2 ],  name = "a") 
+b  =  tf.constant([3, 6 ],  name = "b") 
+x  =  tf.add(a , b ,  name = "add")
+with tf.Session() as sess:
+	writer = tf.summary.FileWriter("./graphs", sess.graph)
+	print(sess.run(x))
+# Close the writer when you're done using it
+writer.flush()
+writer.close()
